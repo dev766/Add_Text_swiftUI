2ee5282be8327ff81f10be450a9e21dae6a5743b
dev766 <akash>
2024-06-27 00:30:16 +0530

    text addeed

---
+++ Add_Text_swiftUI/AddingText.swift
@@ -0,0 +1,18 @@
+//
+//  AddingText.swift
+//  Add_Text_swiftUI
+//
+//  Created by Apple on 27/06/24.
+//
+
+import SwiftUI
+
+struct AddingText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingText()
+}
