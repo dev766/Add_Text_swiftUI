ae3869ff8eada06b8f027d0f00a1424e48fa2f60
dev766 <akash>
2024-06-27 00:30:45 +0530

    test

--- Add_Text_swiftUI/AddingText.swift
+++ Add_Text_swiftUI/AddingText.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 struct AddingText: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Placeholder here is the one and only akash apatil  the hero of tha day")
+//            .font(.largeTitle)
+//            .fontWeight(.semibold)
+//            .foregroundColor(.orange)
+//            .fontWeight(.bold)
+//            .underline(true, color: .green)
+        
+//            .font(.system(size: 24, weight: .heavy, design: .serif))
+        
+            .multilineTextAlignment(.center)
+//            .baselineOffset(10)
+//            .kerning(0.1)
+            .foregroundColor(.red)
+            .frame(width: 100, height: 100, alignment: .center)
+            
     }
 }
+
 
 #Preview {
     AddingText()
