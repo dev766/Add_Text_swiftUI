51ab02ac41b09722431ee52577db40c2f92a0076
dev766 <akash>
2024-06-27 00:23:46 +0530

    Initial Commit

---
+++ Add_Text_swiftUI/Add_Text_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Add_Text_swiftUIApp.swift
+//  Add_Text_swiftUI
+//
+//  Created by Apple on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Add_Text_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
